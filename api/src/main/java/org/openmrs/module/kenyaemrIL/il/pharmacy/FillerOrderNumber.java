f3f459b9b999eb414f8e59f3805d5f10ccae37b9
Stanslaus Odhiambo <mstans8@gmail.com>
2017-11-21 17:07:34 +0300

    update pharmacy functionality - process pharmacy dispense models

---
+++ api/src/main/java/org/openmrs/module/kenyaemrIL/il/pharmacy/FillerOrderNumber.java
@@ -0,0 +1,26 @@
+package org.openmrs.module.kenyaemrIL.il.pharmacy;
+
+/**
+ * @author Stanslaus Odhiambo
+ *         Created on 21/11/2017.
+ */
+public class FillerOrderNumber {
+    private String entity;
+    private String number;
+
+    public String getEntity() {
+        return entity;
+    }
+
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
