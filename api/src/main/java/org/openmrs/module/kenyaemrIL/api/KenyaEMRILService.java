afa9b88eb027fb4dd3acd643e4f9eb62b2f595bd
Stanslaus Odhiambo <mstans8@gmail.com>
2017-11-21 17:12:15 +0300

    update stubs for pharmacy dispense service methods

--- api/src/main/java/org/openmrs/module/kenyaemrIL/api/KenyaEMRILService.java
+++ api/src/main/java/org/openmrs/module/kenyaemrIL/api/KenyaEMRILService.java
@@ -3,12 +3,12 @@
  * Version 1.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://license.openmrs.org
- *
+ * <p>
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific language governing rights and limitations
  * under the License.
- *
+ * <p>
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
 package org.openmrs.module.kenyaemrIL.api;
@@ -16,6 +16,7 @@ package org.openmrs.module.kenyaemrIL.api;
 import org.openmrs.api.OpenmrsService;
 import org.openmrs.module.kenyaemrIL.il.ILAppointment;
 import org.openmrs.module.kenyaemrIL.il.ILPerson;
+import org.openmrs.module.kenyaemrIL.il.pharmacy.ILPharmacyDispense;
 import org.openmrs.module.kenyaemrIL.il.pharmacy.ILPharmacyOrder;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,83 +29,95 @@ import java.util.List;
  * <code>
  * Context.getService(KenyaEMRILService.class).someMethod();
  * </code>
- * 
+ *
  * @see org.openmrs.api.context.Context
  */
 @Transactional
 public interface KenyaEMRILService extends OpenmrsService {
      
 	/*
-	 * Add service methods here
+     * Add service methods here
 	 * 
 	 */
 
     /**
      * Processes and returns a list of available IL Person records received from the IL, typically ADTA04 and ADTA08
+     *
      * @param status - boolean value showing whether or not to fetch records that have been processed
      * @return a list of @{@link ILPerson} records satisfying the given criteria
      */
-	List<ILPerson> getPersonList(boolean status);
+    List<ILPerson> getPersonList(boolean status);
 
-	List<ILPerson> getAddPersonList(boolean status);
+    List<ILPerson> getAddPersonList(boolean status);
 
-	List<ILPerson> getUpdatePersonList(boolean status);
+    List<ILPerson> getUpdatePersonList(boolean status);
 
-	boolean sendUpdateRequest(ILPerson ilPerson);
+    boolean sendUpdateRequest(ILPerson ilPerson);
 
-	boolean sendAddPersonRequest(ILPerson ilPerson);
-
-
-	/**
-	 * Fetches all Appointment Instances received in the system via the IL
-	 * @return  a list of appointments @see {@link ILAppointment}
-	 */
-	List<ILAppointment> fetchAllAppointments();
-
-	/**
-	 *
-	 * Fetches a list of appointments based on the processed status
-	 * @param processed -  flag for the criteria filtering, @see @{@link org.openmrs.module.kenyaemrIL.KenyaEmrInbox  status}
-	 * @return
-	 */
-	List<ILAppointment> fetchAppointments(boolean processed);
-
-	/**
-	 * Deletes/Cancels and appointment
-	 * @param ilAppointment appointment to cancel
-	 * @return true if successful, false otherwise
-	 */
-	boolean deleteAppointment(ILAppointment ilAppointment);
-
-	/**
-	 * Creates an appointment
-	 * @param ilAppointment - the appointment to create
-	 * @return the created appointment
-	 */
-	ILAppointment createAppointment(ILAppointment ilAppointment);
-
-	/**
-	 * Updates an appointment
-	 * @param ilAppointment - the appointment to uppdate
-	 * @return the updated appointment
-	 */
-	ILAppointment updateAppointment(ILAppointment ilAppointment);
+    boolean sendAddPersonRequest(ILPerson ilPerson);
 
 
-	List<ILPharmacyOrder> fetchAllPharmacyOrders();
-	List<ILPharmacyOrder> fetchPharmacyOrders(boolean processed);
-	ILPharmacyOrder createPharmacyOrder(ILPharmacyOrder ilPharmacyOrder);
-	ILPharmacyOrder updatePharmacyOrder(ILPharmacyOrder ilPharmacyOrder);
-	boolean deletePharmacyOrder(ILPharmacyOrder ilPharmacyOrder);
+    /**
+     * Fetches all Appointment Instances received in the system via the IL
+     *
+     * @return a list of appointments @see {@link ILAppointment}
+     */
+    List<ILAppointment> fetchAllAppointments();
+
+    /**
+     * Fetches a list of appointments based on the processed status
+     *
+     * @param processed -  flag for the criteria filtering, @see @{@link org.openmrs.module.kenyaemrIL.KenyaEmrInbox  status}
+     * @return
+     */
+    List<ILAppointment> fetchAppointments(boolean processed);
+
+    /**
+     * Deletes/Cancels and appointment
+     *
+     * @param ilAppointment appointment to cancel
+     * @return true if successful, false otherwise
+     */
+    boolean deleteAppointment(ILAppointment ilAppointment);
+
+    /**
+     * Creates an appointment
+     *
+     * @param ilAppointment - the appointment to create
+     * @return the created appointment
+     */
+    ILAppointment createAppointment(ILAppointment ilAppointment);
+
+    /**
+     * Updates an appointment
+     *
+     * @param ilAppointment - the appointment to uppdate
+     * @return the updated appointment
+     */
+    ILAppointment updateAppointment(ILAppointment ilAppointment);
+
+    /*    Pharmacy Orders     */
+    List<ILPharmacyOrder> fetchAllPharmacyOrders();
+
+    List<ILPharmacyOrder> fetchPharmacyOrders(boolean processed);
+
+    ILPharmacyOrder createPharmacyOrder(ILPharmacyOrder ilPharmacyOrder);
+
+    ILPharmacyOrder updatePharmacyOrder(ILPharmacyOrder ilPharmacyOrder);
+
+    boolean deletePharmacyOrder(ILPharmacyOrder ilPharmacyOrder);
 
 
+    //    Pharmacy Dispense
+    List<ILPharmacyDispense> fetchAllPharmacyDispenses();
 
+    List<ILPharmacyDispense> fetchPharmacyDispenses(boolean processed);
 
+    ILPharmacyDispense createPharmacyDispense(ILPharmacyDispense ilPharmacyDispense);
 
+    ILPharmacyDispense updatePharmacyDispense(ILPharmacyDispense ilPharmacyDispense);
 
-
-
-
+    boolean deletePharmacyDispense(ILPharmacyDispense ilPharmacyDispense);
 
 
 }
