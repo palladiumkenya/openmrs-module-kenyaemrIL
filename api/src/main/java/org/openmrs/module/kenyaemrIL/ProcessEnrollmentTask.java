2cbcaf6c76a8f8cc6d1daa3971cac187869567ba
Stanslaus Odhiambo <mstans8@gmail.com>
2018-01-22 10:50:07 +0300

    add the last encounter id

--- api/src/main/java/org/openmrs/module/kenyaemrIL/ProcessEnrollmentTask.java
+++ api/src/main/java/org/openmrs/module/kenyaemrIL/ProcessEnrollmentTask.java
@@ -13,11 +13,14 @@ import org.openmrs.scheduler.tasks.AbstractTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 /**
- * Implementation of a task that processes the IL inbox every one minute .
+ * Implementation of a task that processes enrollments tasks and marks the for sending to IL.
  */
 public class ProcessEnrollmentTask extends AbstractTask {
 
@@ -31,29 +34,36 @@ public class ProcessEnrollmentTask extends AbstractTask {
     @Override
     public void execute() {
         log.info("Executing task at " + new Date());
-// Fetch enrollment encounter
+//        Fetch enrolment encounter
+//        Fetch the last date of fetch
         Date fetchDate = null;
+        String fetchDateString = Context.getAdministrationService().getGlobalProperty("enrolmentTask.lastFetchDateAndTime");
+        DateFormat formatter = new SimpleDateFormat("hhmmssyyyyMMdd");
+        try {
+            fetchDate = formatter.parse(fetchDateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         EncounterType encounterTypeEnrollment = Context.getEncounterService().getEncounterTypeByUuid("de78a6be-bfc5-4634-adc3-5f1a280455cc");
- //Fetch all encounters
-        List<Encounter> pendingEnrollments = fetchPendingEnrollments(encounterTypeEnrollment,fetchDate);
-
-        for(Encounter e : pendingEnrollments){
+        //Fetch all encounters
+        List<Encounter> pendingEnrollments = fetchPendingEnrollments(encounterTypeEnrollment, fetchDate);
+        for (Encounter e : pendingEnrollments) {
             Patient p = e.getPatient();
             registrationEvent(p);
         }
     }
 
     private List<Encounter> fetchPendingEnrollments(EncounterType encounterType, Date date) {
-       // Context.getEncounterService().getEncounters()
-       // Fetch encounters
-       throw new NotYetImplementedException("Not working yet");
+//        TODO - fetch pending enrolments
+        throw new NotYetImplementedException("Not working yet");
     }
 
-    private  void  registrationEvent(Patient patient){
+    private void registrationEvent(Patient patient) {
         ILPerson ilPerson = ILPatientRegistration.iLPatientWrapper(patient);
         KenyaEMRILService service = Context.getService(KenyaEMRILService.class);
         boolean b = service.sendAddPersonRequest(ilPerson);
     }
+
     private KenyaEMRILService getEMRILService() {
         return Context.getService(KenyaEMRILService.class);
     }
