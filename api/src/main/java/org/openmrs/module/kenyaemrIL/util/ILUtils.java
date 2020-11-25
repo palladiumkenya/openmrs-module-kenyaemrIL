c376a8e8a140452825fd6ceb64c34496f4558b58
ojwanganto <ojwangwachiaje@gmail.com>
2020-11-25 16:22:25 +0300

    clean up ILUtils class to remove regimen specific methods

--- api/src/main/java/org/openmrs/module/kenyaemrIL/util/ILUtils.java
+++ api/src/main/java/org/openmrs/module/kenyaemrIL/util/ILUtils.java
@@ -16,20 +16,22 @@ package org.openmrs.module.kenyaemrIL.util;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.openmrs.*;
+import org.openmrs.Encounter;
+import org.openmrs.EncounterType;
+import org.openmrs.Form;
+import org.openmrs.Patient;
+import org.openmrs.Person;
+import org.openmrs.Provider;
+import org.openmrs.User;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.context.Context;
-import org.openmrs.util.OpenmrsUtil;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Miscellaneous utility methods
@@ -201,156 +203,5 @@ public class ILUtils {
 
 		return encounterTypes;
 	}
-
-
-	/**
-	 * Gets mappings for KenyaEMR-Nascop codes drug mapping
-	 * The mapping file is a json array with the following structure:
-	 * {
-	 *  "nascop_code": "AF1A",
-	 *  "drug_name": "TDF+3TC+EFV",
-	 *  "concept_id": 1234,
-	 *  "drug_type":"G",
-	 *  "regimen_line":"First Line"
-	 * }
-	 * @return json array
-	 */
-	public static JSONArray getNacopCodesMapping() {
-
-		File configFile = OpenmrsUtil.getDirectoryInApplicationDataDirectory(Context.getAdministrationService().getGlobalProperty(GP_IL_CONFIG_DIR));
-		String fullFilePath = configFile.getPath() + File.separator + "KenyaEMR_Nascop_Codes_Drugs_Map.json";
-		JSONParser jsonParser = new JSONParser();
-		try {
-			//Read JSON file
-			FileReader reader = new FileReader(fullFilePath);
-			Object obj = jsonParser.parse(reader);
-			JSONArray drugsMap = (JSONArray) obj;
-
-			return drugsMap;
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-
-		return null;
-	}
-
-	/**
-	 * Iterates through the mapping document and returns an item matching on key and value
-	 * @param key to match
-	 * @param value to match
-	 * @return JSONObject
-	 */
-	public static JSONObject getDrugEntryByKeyAndValue(String key, String value) {
-
-		JSONArray config = ILUtils.getNacopCodesMapping();
-		if (config != null) {
-			for (int i = 0; i < config.size(); i++) {
-				JSONObject o = (JSONObject) config.get(i);
-				if (o.get(key).toString().equals(value)) {
-					return o;
-				}
-			}
-		}
-		return null;
-	}
-
-
-	/**
-	 * Helper method for getting a config entry by concept_id
-	 * @param conceptId
-	 * @return
-	 */
-	public static JSONObject getDrugEntryByConceptId(int conceptId) {
-
-		JSONArray config = ILUtils.getNacopCodesMapping();
-		if (config != null) {
-			for (int i = 0; i < config.size(); i++) {
-				JSONObject o = (JSONObject) config.get(i);
-				if ((Integer) o.get("concept_id") == conceptId) {
-					return o;
-				}
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Helper method for getting a config entry by concept_id
-	 * @param conceptId
-	 * @return
-	 */
-	public static JSONObject getDrugEntryByConceptId(int conceptId, JSONArray config) {
-
-		if (config != null) {
-			for (int i = 0; i < config.size(); i++) {
-				JSONObject o = (JSONObject) config.get(i);
-				if (((Long) o.get("concept_id")).intValue() == conceptId) {
-					return o;
-				}
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Helper method for getting a config entry by nascop code
-	 * @param drugName
-	 * @return
-	 */
-	public static JSONObject getDrugEntryByNascopCode(String drugName) {
-
-		JSONArray config = ILUtils.getNacopCodesMapping();
-		if (config != null) {
-			for (int i = 0; i < config.size(); i++) {
-				JSONObject o = (JSONObject) config.get(i);
-				if (o.get("nascop_code").toString().equals(drugName)) {
-					return o;
-				}
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Helper method for getting a config entry by nascop code
-	 * @param drugName
-	 * @return
-	 */
-	public static JSONObject getDrugEntryByNascopCode(String drugName, JSONArray config) {
-
-		if (config != null) {
-			for (int i = 0; i < config.size(); i++) {
-				JSONObject o = (JSONObject) config.get(i);
-				if (o.get("nascop_code").toString().equals(drugName)) {
-					return o;
-				}
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Helper method for getting a config entry by nascop code
-	 * @param drugName
-	 * @return
-	 */
-	public static JSONObject getDrugEntryByDrugName(String drugName, JSONArray config) {
-
-		if (config != null) {
-			for (int i = 0; i < config.size(); i++) {
-				JSONObject o = (JSONObject) config.get(i);
-				if (o.get("drug_name").toString().equals(drugName)) {
-					return o;
-				}
-			}
-		}
-		return null;
-	}
-
 
 }
