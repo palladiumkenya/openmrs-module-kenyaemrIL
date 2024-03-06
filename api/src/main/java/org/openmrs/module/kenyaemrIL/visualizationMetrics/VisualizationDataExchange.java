37ddd95bd80bf97216aa1c2274ae64654c448cc5
patryllus <gpatryllus@gmail.com>
2024-03-06 15:55:22 +0300

    Added billing and payments data extracts to above site portal

--- api/src/main/java/org/openmrs/module/kenyaemrIL/visualizationMetrics/VisualizationDataExchange.java
+++ api/src/main/java/org/openmrs/module/kenyaemrIL/visualizationMetrics/VisualizationDataExchange.java
@@ -2,20 +2,24 @@ package org.openmrs.module.kenyaemrIL.visualizationMetrics;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.Transaction;
+import org.hibernate.jdbc.Work;
 import org.json.simple.JSONObject;
-import org.openmrs.*;
+import org.openmrs.Diagnosis;
+import org.openmrs.Encounter;
+import org.openmrs.Visit;
 import org.openmrs.api.DiagnosisService;
 import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.kenyaemr.metadata.CommonMetadata;
-import org.openmrs.module.kenyaemr.metadata.HivMetadata;
+import org.openmrs.api.db.hibernate.DbSessionFactory;
 import org.openmrs.module.kenyaemrIL.il.utils.MessageHeaderSingleton;
-import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.util.PrivilegeConstants;
 
+import java.sql.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.*;
 
 public class VisualizationDataExchange {
@@ -40,6 +44,8 @@ public class VisualizationDataExchange {
 		List<SimpleObject> diagnosis = new ArrayList<SimpleObject>();
 		List<SimpleObject> workload = new ArrayList<SimpleObject>();
 		List<SimpleObject> billing = new ArrayList<SimpleObject>();
+		List<SimpleObject> billingItems= new ArrayList<SimpleObject>();
+		List<SimpleObject> paymentItems= new ArrayList<SimpleObject>();
 		List<SimpleObject> payments = new ArrayList<SimpleObject>();
 		List<SimpleObject> inventory = new ArrayList<SimpleObject>();
 		List<SimpleObject> mortality = new ArrayList<SimpleObject>();
@@ -77,7 +83,7 @@ public class VisualizationDataExchange {
 			payloadObj.put("visits", visits);
 		}
 
-		diagnosisMap = allDiagnosis();
+		diagnosisMap = allDiagnosis(encounter);
 		if (!diagnosisMap.isEmpty()) {
 			for (Map.Entry<String, Integer> diagnosisEntry : diagnosisMap.entrySet()) {
 				SimpleObject diagnosisObject = new SimpleObject();
@@ -98,28 +104,38 @@ public class VisualizationDataExchange {
 		} else {
 			payloadObj.put("workload", workload);
 		}
-		if (billing.size() > 0) {
-			SimpleObject billingObject = new SimpleObject();
-			billingObject.put("service_type", "");
-			billingObject.put("invoices", "");
-			billingObject.put("amount_due", "");
-			billingObject.put("amount_paid", "");
-			billingObject.put("balance_due", "");
-			billing.add(billingObject);
-			payloadObj.put("billing", billing);
+		billingItems = getBillingItems();
+		if (billingItems.size() > 0) {
+			System.out.println("We have some bills");		
+			for (int i = 0; i < billingItems.size(); i++) {
+				SimpleObject bill = billingItems.get(i);
+				SimpleObject billingObject = new SimpleObject();				
+				billingObject.put("service_type", bill.get("service_type"));
+				billingObject.put("invoices", bill.get("invoices"));
+				billingObject.put("amount_due", bill.get("amount_due"));
+				billingObject.put("amount_paid",bill.get("amount_paid"));
+				billingObject.put("balance_due", bill.get("balance_due"));
+				billing.add(billingObject);
+				payloadObj.put("billing", billing);
+			}
 		} else {
 			payloadObj.put("billing", billing);
 		}
-		if (payments.size() > 0) {
-			SimpleObject paymentsObject = new SimpleObject();
-			paymentsObject.put("payment_mode", "");
-			paymentsObject.put("no_of_patients", "");
-			paymentsObject.put("amount_paid", "");
-			payments.add(paymentsObject);
-			payloadObj.put("payments", payments);
+		paymentItems = getPayments();
+		if (paymentItems.size() > 0) {
+			System.out.println("We have some payments");
+			for (int i = 0; i < paymentItems.size(); i++) {
+				SimpleObject paymentsList= paymentItems.get(i);
+				SimpleObject paymentsObject = new SimpleObject();
+				paymentsObject.put("payment_mode", paymentsList.get("payment_mode"));
+				paymentsObject.put("no_of_patients", paymentsList.get("no_of_patients"));
+				paymentsObject.put("amount_paid", paymentsList.get("amount_paid"));
+				payments.add(paymentsObject);
+				payloadObj.put("payments", payments);
+			}
 		} else {
 			payloadObj.put("payments", payments);
-		}
+		}		
 		if (inventory.size() > 0) {
 			SimpleObject inventoryObject = new SimpleObject();
 			inventoryObject.put("item_name", "");
@@ -163,49 +179,167 @@ public class VisualizationDataExchange {
 		return visitMap;
 	}
 
-	public static Map<String, Integer> allDiagnosis() {
-
-		Map<String, Integer> diagnosisMap = new HashMap<>();
-		// Does not use fetchDate . Sends cumulative data . Expensive
-		//Forms with diagnosis
-		Form hivGreencardForm = MetadataUtils.existing(Form.class, HivMetadata._Form.HIV_GREEN_CARD);
-		Form clinicalEncounterForm = MetadataUtils.existing(Form.class, CommonMetadata._Form.CLINICAL_ENCOUNTER);
-
-		List<Patient> allPatients = Context.getPatientService().getAllPatients();
-		for (Patient patient : allPatients) {
-
-			List<Encounter> encounters = Context.getEncounterService().getEncounters(patient, null,
-				null, null, Arrays.asList(hivGreencardForm, clinicalEncounterForm), null, null, null, null, false);
-			//System.out.println("Count of encounters  ==> " + encounters.size());
-			for (Encounter encounterWithDiagnosis : encounters) {
-				DiagnosisService diagnosisService = Context.getDiagnosisService();
-				List<Diagnosis> allDiagnosis = diagnosisService.getPrimaryDiagnoses(encounterWithDiagnosis);
-				if (!allDiagnosis.isEmpty()) {
-					for (Diagnosis diagnosis : allDiagnosis) {
-						String diagnosisName = diagnosis.getDiagnosis().getCoded().getName().getName();
-						System.out.println("Diagnosis Name : " + diagnosisName);
-						diagnosisMap.put(diagnosisName, diagnosisMap.getOrDefault(diagnosisName, 0) + 1);
-					}
-				}
-			}
-		}
-		return diagnosisMap;
-	}
-// Uses fetchDate . Does not send cumulative data only incremental updates as at fetch date
 //	public static Map<String, Integer> allDiagnosis(Encounter encounter) {
 //
 //		Map<String, Integer> diagnosisMap = new HashMap<>();
-//		DiagnosisService diagnosisService = Context.getDiagnosisService();
-//		List<Diagnosis> allDiagnosis = diagnosisService.getPrimaryDiagnoses(encounter);
-//		if(!allDiagnosis.isEmpty()) {
-//			for (Diagnosis diagnosis : allDiagnosis) {
-//				String diagnosisName = diagnosis.getDiagnosis().getCoded().getName().getName();
-//				System.out.println("Diagnosis Name : " + diagnosisName);
-//				diagnosisMap.put(diagnosisName, diagnosisMap.getOrDefault(diagnosisName, 0) + 1);
+//		// Does not use fetchDate . Sends cumulative data . Expensive
+//		//Forms with diagnosis
+//		Form hivGreencardForm = MetadataUtils.existing(Form.class, HivMetadata._Form.HIV_GREEN_CARD);
+//		Form clinicalEncounterForm = MetadataUtils.existing(Form.class, CommonMetadata._Form.CLINICAL_ENCOUNTER);
+//
+//		List<Patient> allPatients = Context.getPatientService().getAllPatients();
+//		for (Patient patient : allPatients) {
+//
+//			List<Encounter> encounters = Context.getEncounterService().getEncounters(patient, null,
+//				null, null, Arrays.asList(hivGreencardForm, clinicalEncounterForm), null, null, null, null, false);
+//			//System.out.println("Count of encounters  ==> " + encounters.size());
+//			for (Encounter encounterWithDiagnosis : encounters) {
+//				DiagnosisService diagnosisService = Context.getDiagnosisService();
+//				List<Diagnosis> allDiagnosis = diagnosisService.getPrimaryDiagnoses(encounterWithDiagnosis);
+//				if (!allDiagnosis.isEmpty()) {
+//					for (Diagnosis diagnosis : allDiagnosis) {
+//						String diagnosisName = diagnosis.getDiagnosis().getCoded().getName().getName();
+//						System.out.println("Diagnosis Name : " + diagnosisName);
+//						diagnosisMap.put(diagnosisName, diagnosisMap.getOrDefault(diagnosisName, 0) + 1);
+//					}
+//				}
 //			}
 //		}
 //		return diagnosisMap;
 //	}
+ //Uses fetchDate . Does not send cumulative data only incremental updates as at fetch date
+	public static Map<String, Integer> allDiagnosis(Encounter encounter) {
+
+		Map<String, Integer> diagnosisMap = new HashMap<>();
+		DiagnosisService diagnosisService = Context.getDiagnosisService();
+		List<Diagnosis> allDiagnosis = diagnosisService.getPrimaryDiagnoses(encounter);
+		if(!allDiagnosis.isEmpty()) {
+			for (Diagnosis diagnosis : allDiagnosis) {
+				String diagnosisName = diagnosis.getDiagnosis().getCoded().getName().getName();
+				System.out.println("Diagnosis Name : " + diagnosisName);
+				diagnosisMap.put(diagnosisName, diagnosisMap.getOrDefault(diagnosisName, 0) + 1);
+			}
+		}
+		return diagnosisMap;
+	}
+
+	/**
+	 * Gets details of all bills
+	 * @param 
+	 * @return details of all bills
+	 */
+	public static List<SimpleObject> getBillingItems() {
+
+		DbSessionFactory sf = Context.getRegisteredComponents(DbSessionFactory.class).get(0);
+
+		final String sqlSelectQuery = "select cbl.service_id, cbl.bill_id, cbs.name, SUM(cbp.amount), SUM(cbp.amount_tendered), SUM(cbp.amount - cbp.amount_tendered ) from openmrs.cashier_bill_line_item cbl inner join openmrs.cashier_bill_payment cbp on cbl.bill_id = cbp.bill_id inner join openmrs.cashier_billable_service cbs on cbs.service_id = cbl.service_id group by cbl.service_id;";
+		final List<SimpleObject> ret = new ArrayList<SimpleObject>();
+		Transaction tx = null;
+		try {
+
+			tx = sf.getHibernateSessionFactory().getCurrentSession().beginTransaction();
+			final Transaction finalTx = tx;
+			sf.getCurrentSession().doWork(new Work() {
+
+				@Override
+				public void execute(Connection connection) throws SQLException {
+					PreparedStatement statement = connection.prepareStatement(sqlSelectQuery);
+					try {
+
+						ResultSet resultSet = statement.executeQuery();
+						if (resultSet != null) {
+							ResultSetMetaData metaData = resultSet.getMetaData();
+
+							while (resultSet.next()) {
+								Object[] row = new Object[metaData.getColumnCount()];
+								for (int i = 1; i <= metaData.getColumnCount(); i++) {
+									row[i - 1] = resultSet.getObject(i);
+								}
+
+								ret.add(SimpleObject.create(
+									"invoices", row[1] != null ? row[1].toString() : "",
+									"service_type", row[2] != null ? row[2].toString() : "",		
+									"amount_due", row[3] != null ? row[3].toString() : "",
+									"amount_paid", row[4] != null ? row[4].toString() : "",
+									"balance_due", row[5] != null ? row[5].toString() : ""
+								));
+							}
+						}
+						finalTx.commit();
+					} finally {
+						try {
+							if (statement != null) {
+								statement.close();
+							}
+						} catch (Exception e) {
+						}
+					}
+				}
+			});
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Unable to execute query", e);
+		}
+		System.out.println(" Bill details ==> "+ret);
+		return ret;
+	}
+
+	/**
+	 * Gets details of all payments
+	 * @param
+	 * @return details of all payments
+	 */
+	public static List<SimpleObject> getPayments() {
+
+		DbSessionFactory sf = Context.getRegisteredComponents(DbSessionFactory.class).get(0);
+
+		final String sqlSelectQuery = "select cbm.payment_mode_id, cbm.name, count(cb.patient_id), SUM( cbp.amount_tendered) as amount_paid from openmrs.cashier_bill_payment cbp inner join openmrs.cashier_payment_mode cbm on cbm.payment_mode_id = cbp.payment_mode_id inner join openmrs.cashier_bill cb on cb.bill_id = cbp.bill_id group by cbm.payment_mode_id;";
+		final List<SimpleObject> ret = new ArrayList<SimpleObject>();
+		Transaction tx = null;
+		try {
+
+			tx = sf.getHibernateSessionFactory().getCurrentSession().beginTransaction();
+			final Transaction finalTx = tx;
+			sf.getCurrentSession().doWork(new Work() {
+
+				@Override
+				public void execute(Connection connection) throws SQLException {
+					PreparedStatement statement = connection.prepareStatement(sqlSelectQuery);
+					try {
+
+						ResultSet resultSet = statement.executeQuery();
+						if (resultSet != null) {
+							ResultSetMetaData metaData = resultSet.getMetaData();
+
+							while (resultSet.next()) {
+								Object[] row = new Object[metaData.getColumnCount()];
+								for (int i = 1; i <= metaData.getColumnCount(); i++) {
+									row[i - 1] = resultSet.getObject(i);
+								}
+
+								ret.add(SimpleObject.create(
+									"payment_mode", row[1] != null ? row[1].toString() : "",
+									"no_of_patients", row[2] != null ? row[2].toString() : "",
+									"amount_paid", row[3] != null ? row[3].toString() : ""									
+								));
+							}
+						}
+						finalTx.commit();
+					} finally {
+						try {
+							if (statement != null) {
+								statement.close();
+							}
+						} catch (Exception e) {
+						}
+					}
+				}
+			});
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Unable to execute query", e);
+		}
+		System.out.println(" Payment details ==> "+ret);
+		return ret;
+	}
 
 }
 
