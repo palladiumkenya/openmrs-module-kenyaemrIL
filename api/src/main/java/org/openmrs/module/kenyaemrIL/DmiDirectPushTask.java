8e957327d3cb9de1a93e5a7adf2c3e4a1def0f51
patryllus <gpatryllus@gmail.com>
2024-03-17 15:11:41 +0300

    Updating DMI to only send Flagged clients

--- api/src/main/java/org/openmrs/module/kenyaemrIL/DmiDirectPushTask.java
+++ api/src/main/java/org/openmrs/module/kenyaemrIL/DmiDirectPushTask.java
@@ -1,14 +1,15 @@
 package org.openmrs.module.kenyaemrIL;
 
 import org.json.simple.JSONArray;
-import org.openmrs.Encounter;
 import org.openmrs.GlobalProperty;
-import org.openmrs.api.EncounterService;
+import org.openmrs.Visit;
+import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.kenyaemrIL.dmi.DmiDataExchange;
 import org.openmrs.module.kenyaemrIL.dmi.dmiUtils;
 import org.openmrs.scheduler.tasks.AbstractTask;
 import org.openmrs.ui.framework.SimpleObject;
+import org.openmrs.util.OpenmrsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,93 +26,91 @@ import java.util.List;
  */
 public class DmiDirectPushTask extends AbstractTask {
 
-    private static final Logger log = LoggerFactory.getLogger(DmiDirectPushTask.class);
-    private String url = "http://www.google.com:80/index.html";
+	private static final Logger log = LoggerFactory.getLogger(DmiDirectPushTask.class);
+	private String url = "http://www.google.com:80/index.html";
 
-    /**
-     * @see AbstractTask#execute()
-     */
-    public void execute() {
-        System.out.println("DMI DIRECT PUSH: Scheduler started....");
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-       // Fetch the last date of fetch
-        Date fetchDate = null;
-        GlobalProperty globalPropertyObject = Context.getAdministrationService().getGlobalPropertyObject("dmiTask.lastFetchDateAndTime");
+	/**
+	 * @see AbstractTask#execute()
+	 */
+	public void execute() {
+		System.out.println("DMI DIRECT PUSH: Scheduler started....");
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+		// Fetch the last date of fetch
+		Date fetchDate = null;
+		GlobalProperty globalPropertyObject = Context.getAdministrationService().getGlobalPropertyObject("dmiTask.lastFetchDateAndTime");
 
-        try {
-            String ts = globalPropertyObject.getValue().toString();
-            fetchDate = formatter.parse(ts);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		try {
+			String ts = globalPropertyObject.getValue().toString();
+			fetchDate = formatter.parse(ts);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        try {
-            Context.openSession();
+		try {
+			Context.openSession();
 
-            // check first if there is internet connectivity before pushing
-            URLConnection connection = new URL(url).openConnection();
-            connection.connect();
+			// check first if there is internet connectivity before pushing
+			URLConnection connection = new URL(url).openConnection();
+			connection.connect();
 
-            List<Encounter> encounters = getComplaintsAndDiagnosis(fetchDate);
-            for (Encounter encounter : encounters) {
-                if (encounter != null) {
-                    DmiDataExchange dmiDataExchange = new DmiDataExchange();
-                    JSONArray params = dmiDataExchange.generateDMIpostPayload(encounter,fetchDate);
-                    System.out.println("Payload to DMI server ==> "+params);
+			List<Visit> visits = getComplaintsAndDiagnosis(fetchDate);
+			if (visits.size() > 1) {
+				for (Visit visit : visits) {
+					if (visit != null) {
+						DmiDataExchange dmiDataExchange = new DmiDataExchange();
+						JSONArray params = dmiDataExchange.generateDMIpostPayload(visit, fetchDate);
+						System.out.println("Payload to DMI server ==> " + params);
 
-                    try {
-                        SimpleObject results = dmiUtils.sendPOST(params.toJSONString());
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+						try {
+							SimpleObject results = dmiUtils.sendPOST(params.toJSONString());
+						} catch (Exception e) {
+							System.out.println(e.getMessage());
+						}
 
-                }
-            }
+					}
+				}
+			}
 
-            Date nextProcessingDate = new Date();
-            globalPropertyObject.setPropertyValue(formatter.format(nextProcessingDate));
-            Context.getAdministrationService().saveGlobalProperty(globalPropertyObject);
-            Context.flushSession();
-        } catch (IOException ioe) {
+			Date nextProcessingDate = OpenmrsUtil.getLastMomentOfDay(new Date());
+			globalPropertyObject.setPropertyValue(formatter.format(nextProcessingDate));
+			Context.getAdministrationService().saveGlobalProperty(globalPropertyObject);
+			Context.flushSession();
+		} catch (IOException ioe) {
 
-            try {
-                String text = "IL - DMI PUSH: At " + new Date() + " there was connectivity error. ";
-                log.warn(text);
-            } catch (Exception e) {
-                log.error("IL - DMI PUSH: Failed to check internet connectivity", e);
-            }
-        } finally {
-            Context.closeSession();
+			try {
+				String text = "IL - DMI PUSH: At " + new Date() + " there was connectivity error. ";
+				log.warn(text);
+			} catch (Exception e) {
+				log.error("IL - DMI PUSH: Failed to check internet connectivity", e);
+			}
+		} finally {
+			Context.closeSession();
 
-        }
-    }
+		}
+	}
 
-    /**
-     * Gets a list of patients who have had complaints or diagnosis or labs recorded in triage or greencard forms since the last timestamp
-     * @param date last timestamp
-     * @return a list of patients who have had complaints or diagnosis or labs recorded as at the provided timestamp
-     */
-    private List<Encounter> getComplaintsAndDiagnosis (Date date) {
-        SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String effectiveDate = sd.format(date);
-        StringBuilder q = new StringBuilder();
-        q.append("select e.encounter_id ");
-        q.append("from encounter e\n" +
-                "  inner join  obs ob on ob.encounter_id = e.encounter_id\n" +
-                "  inner join  encounter_type et on et.encounter_type_id = e.encounter_type and et.uuid in ('d1059fb9-a079-4feb-a749-eedd709ae542','a0034eee-1940-4e35-847f-97537a35d05e','e1406e88-e9a9-11e8-9f32-f2801f1b9fd1')\n" +
-                "  left join orders od on  od.previous_order_id = ob.order_id and od.order_type_id = 3 and od.order_action='DISCONTINUE'");
-        q.append("where e.date_created >= '" + effectiveDate + "' or e.date_changed >= '" + effectiveDate + "'");
-        q.append(" and ((ob.encounter_id is not null and ob.concept_id in (5219,6042) and ob.voided=0)  or od.encounter_id is not null)");
+	/**
+	 * Gets a list of patients who have had complaints or diagnosis or labs recorded in triage or greencard forms since the last timestamp
+	 * @param date last timestamp
+	 * @return a list of patients who have had complaints or diagnosis or labs recorded as at the provided timestamp
+	 */
+	private List<Visit> getComplaintsAndDiagnosis (Date date) {
+		SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String effectiveDate = sd.format(date);
+		StringBuilder q = new StringBuilder();
+		q.append("select v.visit_id ");
+		q.append("from visit v " );
+		q.append("where v.date_created > '" + effectiveDate + "' ");
 
-        List<Encounter> encounters = new ArrayList<>();
-        EncounterService encounterService = Context.getEncounterService();
-        List<List<Object>> queryData = Context.getAdministrationService().executeSQL(q.toString(), true);
-        for (List<Object> row : queryData) {
-            Integer encounterId = (Integer) row.get(0);
-            Encounter e = encounterService.getEncounter(encounterId);
-            encounters.add(e);
-        }
-        return encounters;
+		List<Visit> visits = new ArrayList<>();
+		VisitService visitService = Context.getVisitService();
+		List<List<Object>> queryData = Context.getAdministrationService().executeSQL(q.toString(), true);
+		for (List<Object> row : queryData) {
+			Integer visitId = (Integer) row.get(0);
+			Visit e = visitService.getVisit(visitId);
+			visits.add(e);
+		}
+		return visits;
 
-    }
+	}
 }
